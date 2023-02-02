f51905151b250694a4cce6dc939b6af1505f0d62
navaneethmadhav <madnav114@gmail.com>
2023-02-02 14:08:58 +0530

    Reviews and Operating Hours

--- src/components/Restop.js
+++ src/components/Restop.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-function Restop() {
+function Restop({op}) {
+  console.log(op);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>Restop</div>
+    <div>
+      <Button variant="warning" className='mt-4' onClick={handleShow}>
+        Opening Times
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Operating Hours</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ListGroup>
+            <ListGroup.Item>Monday : {op.Monday}</ListGroup.Item>
+            <ListGroup.Item>Tuesday : {op.Tuesday}</ListGroup.Item>
+            <ListGroup.Item>Wednesday : {op.Wednesday}</ListGroup.Item>
+            <ListGroup.Item>Thurday : {op.Thursday}</ListGroup.Item>
+            <ListGroup.Item>Friday : {op.Friday}</ListGroup.Item>
+            <ListGroup.Item>Saturday : {op.Saturday}</ListGroup.Item>
+            <ListGroup.Item>Sunday : {op.Sunday}</ListGroup.Item>
+          </ListGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   )
 }
 
--- src/components/Viewrest.js
+++ src/components/Viewrest.js
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image'
 import ListGroup from 'react-bootstrap/ListGroup';
 import Restop from './Restop';
+import Restreview from './Restreview';
 
 function Viewrest() {
     const urlParams=useParams()
@@ -36,10 +37,10 @@ function Viewrest() {
 
             viewrest?(
                 <Row>
-                    <Col>
+                    <Col md={3} className='mt-3'>
                         <Image src={viewrest.photograph}  fluid/>
                     </Col>
-                    <Col className='mt-5'>
+                    <Col md={8} className='mt-3'>
                         <h1 className='mb-4'>{viewrest.name}</h1>
                         <ListGroup>
                             <ListGroup.Item variant="dark">Id : {viewrest.id}</ListGroup.Item>
@@ -48,9 +49,10 @@ function Viewrest() {
                             <ListGroup.Item variant="dark">Neighborhood : {viewrest.neighborhood}</ListGroup.Item>
                             <ListGroup.Item variant="dark">Cuisine type : {viewrest.cuisine_type}</ListGroup.Item>
                             <ListGroup.Item variant="dark">Latitude and Longitude : </ListGroup.Item>
-                            <ListGroup.Item variant="dark">Operating Hours</ListGroup.Item>
-                            <ListGroup.Item variant="dark">Reviews : </ListGroup.Item>
+                            
                         </ListGroup>
+                        <><Restop op={viewrest.operating_hours}/></>
+                            <><Restreview review={viewrest.reviews}/></>
                     </Col>
                 </Row>
             ):'null'
